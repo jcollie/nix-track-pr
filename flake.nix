74b29029e0c6048b0612abcf8c6109e05d920da2
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-11-03 11:58:28 -0600

    update

--- flake.nix
+++ flake.nix
@@ -7,36 +7,41 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    packages = system:
-      import nixpkgs {
-        inherit system;
-      };
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ] (system: function (packages system));
-  in {
-    packages = forAllSystems (pkgs: {
-      default = self.packages.${pkgs.system}.nix-track-pr;
-      nix-track-pr = pkgs.callPackage ./package.nix {};
-    });
-    devShells = forAllSystems (pkgs: {
-      default = self.devShells.${pkgs.system}.zig_0_15;
-      zig_0_15 = pkgs.mkShell {
-        name = "nix-track-pr";
-        nativeBuildInputs = [
-          pkgs.zig_0_15
-          pkgs.pinact
-        ];
-      };
-    });
-  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      packages =
+        system:
+        import nixpkgs {
+          inherit system;
+        };
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ] (system: function (packages system));
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = self.packages.${pkgs.stdenv.hostPlatform.system}.nix-track-pr;
+        nix-track-pr = pkgs.callPackage ./package.nix { };
+      });
+      devShells = forAllSystems (pkgs: {
+        default = self.devShells.${pkgs.stdenv.hostPlatform.system}.zig_0_15;
+        zig_0_15 = pkgs.mkShell {
+          name = "nix-track-pr";
+          nativeBuildInputs = [
+            pkgs.zig_0_15
+            pkgs.pinact
+          ];
+        };
+      });
+    };
 }
